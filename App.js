9dcc99c363e287aef8a829351b5bc6e55e9f4719
Leonardo <furiossam@edu.univali.br>
2021-05-06 22:19:22 -0300

    Mudanças no layout da tela de login e base da autenticação

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import color from 'color';
+import api from "./src/services/api";
 
 export default class App extends Component {
   render() {
@@ -10,8 +14,24 @@ export default class App extends Component {
           <Text style={styles.loginTitle}>Faça Login no sistema HealthRoutes</Text>
           <StatusBar style="auto" />
         </View>
+        <Input style={styles.input}
+            placeholder='Username'
+            leftIcon={
+              <Icon
+                name='user'
+                size={24}
+                color='black'
+              />
+            }
+          />
+          <Input style={styles.input}
+            placeholder='Password'
+            leftIcon={{ type: 'font-awesome', name: 'unlock-alt' }}
+            color = 'black'
+            size={24}
+            secureTextEntry={true}
+          />
       </View>
-      
     );
   }
   
@@ -26,15 +46,20 @@ const styles = StyleSheet.create({
   },
   containerHeader: {
     marginTop: -600,
-    paddingTop: 20,
+    paddingTop: 500,
     backgroundColor: '#237582',
     alignItems: 'center',
     justifyContent: 'center',
   },
   loginTitle: {
+    paddingTop: 60,
     fontSize: 20,
     textAlign: "center",
     margin: 50,
+    color: "#FFF"
+  },
+  input: {
+    fontSize: 20,
     color: "#FFF"
   }
 });
---
+++ src/services/api.js
@@ -0,0 +1,111 @@
+import { create } from "apisauce";
+
+// const baseURL = "http://localhost:8090"; // LOCAL
+const baseURL = "http://10.0.0.105:8090"; // PC LOCAL
+// const baseURL = "http://192.168.15.9:8090"; // PC LOCAL
+
+let token = null;
+
+const api = create({
+  baseURL,
+  timeout: 300000, // 5 min,
+});
+
+const requestTransform = (request) => {
+  if (token) {
+    request.headers.authorization = `${token}`;
+  }
+};
+
+const naviMonitor = (response) => {
+  const { status, headers } = response;
+  if (status === 401) {
+    logout();
+    // if (response.config.url !== "/login") {
+    //   navigate(paths.Landing);
+    // }
+  }
+
+  if (headers.authorization) {
+    token = headers.authorization;
+  }
+};
+
+// BEARER AUTHENTICATION
+api.addRequestTransform(requestTransform);
+// GET THE ACCESS TOKEN FROM REQS
+api.addMonitor(naviMonitor);
+
+const logout = () => {
+  token = null;
+};
+
+const login = async (body) => {
+  logout();
+  return await api.post("/login", body);
+};
+
+// const checkIfExists = async (barcode) => {
+//   return await api.get("/product/exists/" + barcode);
+// }
+
+// const saveProduct = async (data) => {
+//   return await api.post("/product", data);
+// }
+
+// const savePriceProduct = async (data) => {
+//   return await api.post("/price", data);
+// }
+
+// const searchProduct = async (barcode) => {
+//   return await api.get("/product/" + barcode);
+// }
+
+// const searchProducts = async (name) => {
+//   return await api.get("/product" + (name ? "?name=" + name : ""));
+// }
+
+// const checkIfExistsProduct = async (barcode) => {
+//   return await api.get("/product/exists/" + barcode);
+// }
+
+// const searchShoppingList = async () => {
+//   return await api.get("/shoppinglist");
+// }
+
+// const createShoppingList = async (data) => {
+//   return await api.post("/shoppinglist", data);
+// }
+
+// const deleteShoppingList = async (id) => {
+//   return await api.delete("/shoppinglist/" + id);
+// }
+
+// const searchShoppingListProducts = async (id) => {
+//   return await api.get("/shoppinglist/" + id + "/products");
+// }
+
+// const addProductToShoppingList = async (id, data) => {
+//   return await api.post("/shoppinglist/" + id + "/product", data);
+// }
+
+// const deleteProductFromShoppingList = async (id, barcode) => {
+//   return await api.delete("/shoppinglist/" + id + "/product/" + barcode);
+// }
+
+export default {
+  logout,
+  login,
+//   checkIfExists,
+//   saveProduct,
+//   savePriceProduct,
+//   searchProduct,
+//   searchProducts,
+//   checkIfExistsProduct,
+//   searchShoppingList,
+//   createShoppingList,
+//   deleteShoppingList,
+//   searchShoppingListProducts,
+//   addProductToShoppingList,
+//   deleteProductFromShoppingList
+};
